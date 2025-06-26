0ecf9f762c7a84d62a2382a31bdf0e10e009a6ef
Arthur S Dutra <arthurdutra@live.ca>
2025-06-26 17:18:09 -0600

    Update dependencies and dev script options

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/Chess-game/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
-})
+  build: {
+    target: 'baseline-widely-available',
+  },
+});
